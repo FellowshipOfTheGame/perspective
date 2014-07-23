65bb9f5b957942068777dc34aca5e8547b05ab58
Victor Moneratto <victor.moneratto@gmail.com>
2014-07-23 00:34:10 -0300

    Player rotates on when turning

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,54 +1,59 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
     public float AccelerationOnGround = 100f;
-    public float MaxVelocityOnGround = 50f;
+    public float MaxHorizontalVelocity = 50f;
 
     public float JumpHeight = 2.01f;
     public float AccelerationOnAir = 15f;
-    //public float MaxVelocityOnAir = 10f;
 
     private bool _isGrounded;
-    
+    public float TurningSpeed = 3;
 
     public void FixedUpdate()
     {
         _isGrounded = Physics.Raycast(transform.position, Vector3.down, collider.bounds.extents.y);
 
-        float acceleration;
-        float maxVelocity;
-        Vector3 velocity = rigidbody.velocity;
-        if (_isGrounded)
-        {
-            acceleration = AccelerationOnGround;
-            maxVelocity = MaxVelocityOnGround;
-
-            if (Input.GetButton("Jump"))
-            {
-                velocity.y = Mathf.Sqrt(-2f * Physics.gravity.y * JumpHeight);
-            }
-        }
-        else
-        {
-            acceleration = AccelerationOnAir;
-            maxVelocity = Mathf.Infinity;
-        }
+        float acceleration = _isGrounded ? AccelerationOnGround : AccelerationOnAir;
 
         float horizontalInput = Input.GetAxis("Horizontal");
 
         bool pathBlocked = Physics.Raycast(
-            origin: transform.position, 
+            origin: transform.position,
             direction: Mathf.Sign(horizontalInput)*Vector3.right,
             distance: collider.bounds.extents.x);
-        
+
         if (!pathBlocked)
         {
             rigidbody.AddForce(horizontalInput*acceleration*Vector3.right);
-            velocity.x = Mathf.Clamp(velocity.x, -maxVelocity, maxVelocity);
         }
 
+        Vector3 velocity = rigidbody.velocity;
+
+        if (_isGrounded)
+        {
+            if (Input.GetButton("Jump"))
+            {
+                velocity.y = Mathf.Sqrt(-2f*Physics.gravity.y*JumpHeight);
+            }
+        }
+
+        velocity.x = Mathf.Clamp(velocity.x, -MaxHorizontalVelocity, MaxHorizontalVelocity);
         rigidbody.velocity = velocity;
+
+        Vector3 euler = transform.rotation.eulerAngles;
+        if (horizontalInput < 0f)
+        {
+            euler.y += 180f*Time.deltaTime*TurningSpeed;
+        }
+        else if (horizontalInput > 0f)
+        {
+            euler.y -= 180f*Time.deltaTime*TurningSpeed;
+        }
+
+        euler.y = Mathf.Clamp(euler.y, 0f, 180f);
+        transform.rotation = Quaternion.Euler(euler);
     }
-    
 }
