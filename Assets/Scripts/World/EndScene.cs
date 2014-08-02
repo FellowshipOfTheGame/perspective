e8b7950edd83d005a0c82072dd73ad99a931d043
Victor Moneratto <victor.moneratto@gmail.com>
2014-08-02 15:04:09 -0300

    EndScene script without fade out

---
+++ Assets/Scripts/World/EndScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+[RequireComponent(typeof (Collider))]
+public class EndScene : MonoBehaviour
+{
+    public string NextScene;
+    public float FadeOutTime;
+    public Color FadeOutColor;
+    private bool _isEnding;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            _isEnding = true;
+            StartCoroutine(FadeScene());
+        }
+        
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            _isEnding = false;
+        }
+    }
+
+    public IEnumerator FadeScene()
+    {
+        yield return new WaitForSeconds(FadeOutTime);
+
+        if (_isEnding)
+        {
+            Application.LoadLevel(NextScene);
+        }
+    }
+
+    
+}
