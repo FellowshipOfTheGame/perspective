2a2e6f0de01e2f6d95c555cc98970c05d74196e9
Victor Moneratto <victor.moneratto@gmail.com>
2014-08-03 23:19:10 -0300

    A bit of camera tinkering

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,14 +2,43 @@
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform Target;
-    public Vector3 Distance = 2f*Vector3.back;
-    public float SmoothTime = .2f;
+    [SerializeField]
+    private Transform _target;
+    public Transform Target
+    {
+        get { return _target; }
+        set
+        {
+            _target = value;
+            _targetRigidbody = _target.rigidbody;
+            _targetMotor = _target.GetComponent<CharacterMotor>();
+            _ahead = Vector3.zero;
+            _velocity = Vector3.zero;
+        }
+    }
+
+    public Vector3 Distance = 2f * Vector3.back;
+    public float SmoothTime = 2f;
+    public float HorizontalAheadOfTime = 2f;
 
     private Vector3 _velocity;
+    private Vector3 _ahead;
+
+    private Rigidbody _targetRigidbody;
+    private CharacterMotor _targetMotor;
 
     public void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, Target.transform.position + Distance, ref _velocity, SmoothTime);
+        _ahead =
+            Vector3.SmoothDamp(_ahead,
+                Vector3.right*HorizontalAheadOfTime*_targetRigidbody.velocity.x/_targetMotor.MaxHorizontalVelocity,
+                ref _velocity,
+                SmoothTime*Time.deltaTime);
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            Target.transform.position + Distance + _ahead,
+            ref _velocity,
+            SmoothTime*Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Utility/DebugUtility.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class DebugUtility
+{
+    [System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void DrawRay(Vector3 origin, Vector3 direction, Color color)
+    {
+        Debug.DrawRay(origin, direction, color, 0f, true);
+    }
+}
