d7600bd5b10940cef86da7047d0b115734686afb
gfceccon <gustavofceccon@live.com>
2014-08-09 21:42:41 -0300

    TNT and TNT scripts

---
+++ Assets/Scripts/World/Tiles/TNT.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Rigidbody))]
+public class TNT : MonoBehaviour
+{
+    public float ExplosionRadius = 1.0f;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contat in collision.contacts)
+        {
+            if (contat.otherCollider.gameObject.CompareTag("Player"))
+            {
+                ExplosionHandler handler = this.GetComponent<ExplosionHandler>();
+                if (handler.Enabled)
+                {
+                    GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+                    foreach (GameObject player in players)
+                    {
+                        if (!player.GetComponent<PlayerDeath>().IsAlive) continue;
+                        handler.OnExplosion(player);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
